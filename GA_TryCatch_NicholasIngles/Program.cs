bd59a10d2898a3470a8eb300fd4b869cfa8b2c36
Nicholas Ingles <nringles@student.rtc.edu>
2024-02-10 22:26:10 -0800

    Errors caught

--- GA_TryCatch_NicholasIngles/Program.cs
+++ GA_TryCatch_NicholasIngles/Program.cs
@@ -1,2 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int number = 0; 
+        bool error = false;
+
+        try
+        {
+           
+            Console.Write("What is your age? Please enter a proper whole integer: ");
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+
+        } 
+        catch (FormatException fex)
+        {
+           
+            Console.WriteLine("Invalid age input. Try again.");
+            error = true;
+        }
+        catch (Exception ex)
+        {
+            
+            Console.WriteLine("Unexpected error occured. Try again.");
+            error = true;
+        }
+
+        if (error == true)
+        {
+            
+        }
+        else
+        {
+            Console.WriteLine("Wow, you are " + number + "? Awesome, welcome!");
+        }
+
+
+
+
+    }
+}
